5b849b888f4b1fac91a96d2c8c77a0b2a8ce16b7
GoodSir6 <eduardo.bomsenhor.info@gmail.com>
2023-12-07 08:16:41 -0300

    commitFInal

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-function adicionarTarefa() {
-  let tarefaBotao = document.getElementById("tarefaBotao");
-  let listaTarefas = document.getElementById("listaTarefas");
-  if (tarefaBotao.value !== "") {
-    let itemTarefa = document.createElement("li");
-    itemTarefa.innerHTML = `
-    <span>${tarefaBotao.value}</span>
-    <button onclick="removerTarefa(this.parentNode)">Remover</button>
-  `;
-    itemTarefa.onclick = function() {
-      marcarCompleta(this);
-    };
-    listaTarefas.appendChild(itemTarefa);
-    tarefaBotao.value = "";
+function addTarefa() {
+    let tarefaBotao = document.getElementById("tarefaBotao");
+    let listaTarefas = document.getElementById("listaTarefas");
+    if (tarefaBotao.value !== "") { // se o usuário não colocar nada no campo de tarefa, vai dar o else
+      let itemTarefa = document.createElement("li"); //cria uma LI com esse conteúdo dentro
+      itemTarefa.innerHTML = `
+  <span>${tarefaBotao.value}</span>
+  <button onclick="removerTarefa(this.parentNode)">Remover</button>`; //adiciona a tarefa com o botão remover na direita, o parâmetro "this.parentNode" é a responsável por definir ao clicar no botão remover, qual será a tarefa removida.
+      listaTarefas.appendChild(itemTarefa);
+      tarefaBotao.value = ""; //é o responsável por quando o usuário clicar em "Adicionar tarefa", deixará o campo em branco novamente.
+    }
+    else {
+      alert("Sua tarefa precisa ter um nome!")
+    }
   }
-  else{
-    alert("Sua tarefa precisa ter um nome!")
+  function removerTarefa(itemTarefa) { //é a função responsável por remover a tarefa.
+    let listaTarefas = document.getElementById("listaTarefas");
+    listaTarefas.removeChild(itemTarefa);
   }
-}
-function marcarCompleta(itemTarefa) {
-  itemTarefa.classList.toggle("completed");
-}
-
-function removerTarefa(itemTarefa) {
-  let listaTarefas = document.getElementById("listaTarefas");
-  listaTarefas.removeChild(itemTarefa);
-}
+  const enter = document.getElementById("tarefaBotao"); //É o responsável por ao apertar enter, a tarefa ser adicionada à lista
+  enter.addEventListener("keydown", function (e) {
+    if (e.key == 'Enter') {
+      addTarefa();
+    }
+  });
